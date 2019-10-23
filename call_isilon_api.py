0095ea5afe288f32f6474550b2bf6a7336a8d845
kyeongrok <oceanfog1@gmail.com>
2019-10-23 15:16:09 +0900

    add byte to tera

--- call_isilon_api.py
+++ call_isilon_api.py
@@ -5,23 +5,27 @@ def getPassword():
 	# 인증서버에 공개키를 등록 해놓고 디코딩 해서 password를 만든다.
 	return ""
 
+def convertByteToTera(byte):
+	return byte / 1024 / 1024 / 1024
+
 def getApiResponse(query):
 	# id와 pw를 바꾼다.
-	auth = str.encode("%s:%s" % ('root', ''))
+	auth = str.encode("%s:%s" % ('root', 'a'))
 	user_and_pass = base64.b64encode(auth).decode("ascii")
 	headers = {"Authorization":"Basic {}".format(user_and_pass),
 			   "Accept":"application/json"}
-	conn = http.client.HTTPSConnection('10.230.101.102', 8080, context=ssl._create_unverified_context())
+	conn = http.client.HTTPSConnection('10.35.106.35', 8080, context=ssl._create_unverified_context())
 	conn.request("GET", query, headers=headers)
 	res = conn.getresponse()
 	return res
 
 #/platform/1/statistics/current?key=node.sysfs.root.bytes.used
 #/platform/1/statistics/current?key=node.sysfs.var.percent.free
-res = getApiResponse("/platform/5/statistics/current?key=cluster.alert.info")
+# res = getApiResponse("/platform/5/statistics/current?key=cluster.alert.info")
+res = getApiResponse("/platform/5/statistics/current?key=ifs.bytes.avail")
 data = res.read()
 result = json.loads(data.decode('utf-8'))
 #print(res.status, res.reason)
 #print(res.getheaders())
 print(result)
-
+print(convertByteToTera(result['stats'][0]['value']))
