e91f84fef3a1d4d11a684ebcbeeae22782e77c7d
kyeongrok <oceanfog1@gmail.com>
2019-10-23 15:31:16 +0900

    add init

--- call_isilon_api.py
+++ call_isilon_api.py
@@ -9,21 +9,28 @@ def convertByteToTera(byte):
 	return byte / 1000 / 1000 / 1000 / 1000
 
 class ApiCaller():
-	def __init__(self):
+	host = "10.35.106.35"
+	user = "root"
+	password = "a"
+
+	def __init__(self, host, user, password):
+		self.host = host
+		self.user = user
+		self.password = password
 		pass
 
 	def getApiResponse(self, query):
 		# id와 pw를 바꾼다.
-		auth = str.encode("%s:%s" % ('root', 'a'))
+		auth = str.encode("%s:%s" % (self.user, self.password))
 		user_and_pass = base64.b64encode(auth).decode("ascii")
 		headers = {"Authorization":"Basic {}".format(user_and_pass),
 				   "Accept":"application/json"}
-		conn = http.client.HTTPSConnection('10.35.106.35', 8080, context=ssl._create_unverified_context())
+		conn = http.client.HTTPSConnection(self.host, 8080, context=ssl._create_unverified_context())
 		conn.request("GET", query, headers=headers)
 		res = conn.getresponse()
 		return res
 
-apiCaller = ApiCaller()
+apiCaller = ApiCaller("10.35.106.35", "root", "a")
 
 #/platform/1/statistics/current?key=node.sysfs.root.bytes.used
 #/platform/1/statistics/current?key=node.sysfs.var.percent.free
