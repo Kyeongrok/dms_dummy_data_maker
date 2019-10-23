235ff4180926474b6efb9b47bbb5b00af203b943
kyeongrok <oceanfog1@gmail.com>
2019-10-23 16:10:07 +0900

    update_isilon_info

--- update_isilon_info.py
+++ update_isilon_info.py
@@ -6,11 +6,11 @@ import time
 from datetime import datetime
 from elasticsearch import Elasticsearch
 import json
+from call_isilon_api import ApiCaller
 
-es = Elasticsearch(hosts=["ec2-54-180-123-238.ap-northeast-2.compute.amazonaws.com"])
-# es = Elasticsearch()
-
-# es.indices.create(index="isilon_info")
+# es = Elasticsearch(hosts=["ec2-54-180-123-238.ap-northeast-2.compute.amazonaws.com"])
+es = Elasticsearch()
+apiCaller = ApiCaller("10.35.106.35", "root", "a")
 
 def getIsilonVolume():
     # isilon에서 데이터를 가져옴
@@ -20,14 +20,11 @@ def getIsilonVolume():
 volume = getIsilonVolume()
 
 doc = makeIsilonInfo(volume)
-doc = {
-        "id":uuid.uuid1(),
-        "cluster_id":"cluster01",
-        "percentage":65,
-        "volume":volume
-    }
-res = es.create(index="isilon_info", id=doc['id'], body=doc)
+doc = apiCaller.getAvailFreeTotal()
 
+#es.indices.create(index="space_info")
+res = es.create(index="space_info", id=doc['id'], body=doc)
+print(res)
 # INGEST하면 용량올라가는 기능
 # 전체 용량 dash board
 # to be dashboard 더미데이터 생성
